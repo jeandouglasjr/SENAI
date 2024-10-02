5365198809e7a447aaa73ba254668d0f23ef87c3
jeandouglasjr <suporte@jeandouglas.com.br>
2024-10-01 21:30:02 -0300

    241001

--- 241001/241001b.js
+++ 241001/241001b.js
@@ -1,9 +1,13 @@
 let numero = prompt(`Digite o número para saber sua tabuada: `)
 let fatorMultiplicacao = prompt(`Qual o fator máximo de multiplicação?`)
-function tabuada(numero){
+function tabuada(numero, fatorMultiplicacao){
     for(i = 1; i <= fatorMultiplicacao; i++){
         let num = i * numero
         console.log(`${numero} X ${i} = ${num}\n`);
     }
 }
-tabuada(numero)
+tabuada(numero, fatorMultiplicacao)
+
+// como utilizar sem a variável global? exemplo
+// mais informações sobre o return
+// repetir explicação das 2 funções
