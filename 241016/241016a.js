62b5dc271e6d8e1ff788b4e295199da76441dddd
jeandouglasjr <suporte@jeandouglas.com.br>
2024-10-17 19:42:53 -0300

    241016

--- 241016/241016a.js
+++ 241016/241016a.js
@@ -6,18 +6,6 @@
 // entrada de dados quando for digitado um número negativo ou zero. Calcule a média dos números positivos
 // digitados.
 
-// let contador = 1, soma = 0, media = 0
-// let numero = Number(prompt(`Digite o ${contador}º número:`))
-// while (numero > 0){
-//     soma = soma + numero
-//     media = soma/contador
-//     numero = Number(prompt(`Digite o ${contador}º número:`))
-//     contador++
-// }
-// console.log(`Contador: ${contador - 1}`)
-// console.log(`Soma: ${soma}`)
-// console.log(`Média: ${media}`)
-
 let contador = 1, soma = 0, media = 0
 let numero = 1
 while (numero > 0){
