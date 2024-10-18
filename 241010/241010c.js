acc1df4e5adbb6bae9f873e96936d18898afd40c
jeandouglasjr <suporte@jeandouglas.com.br>
2024-10-17 21:58:32 -0300

    241010

--- 241010/241010c.js
+++ 241010/241010c.js
@@ -2,24 +2,25 @@
 // Aluno: Jean Douglas Toledo Rodrigues Junior
 // Exercício 4
 
-// Os números primos possuem várias aplicações dentro da Computação, por exemplo, na criptografia. Um número primo é aquele que é divisível
-// apenas por um e por ele mesmo. Faça um programa que peça ao usuário para digitar um número e informe se ele é primo. Depois implemente um
-// laço de repetição para solicitar cinco números inteiros e mostre na tela se são primos ou não.
+// Os números primos possuem várias aplicações dentro da Computação, por exemplo, na criptografia. Um número
+// primo é aquele que é divisível apenas por um e por ele mesmo. Faça um programa que peça ao usuário para
+// digitar um número e informe se ele é primo. Depois implemente um laço de repetição para solicitar cinco
+// números inteiros e mostre na tela se são primos ou não.
 
-let contador = Number(1)
-let soma = Number(0)
-let numero = Number(prompt(`Digite um número qualquer:`))
-while(numero <= 0){
-    numero = Number(prompt(`Digite um número válido (positivo e acima de zero):\n`))
+let numero = Number(prompt(`Digite um número inteiro, positivo e maior que zero:`))
+while (numero <=0){
+    numero = Number(prompt(`Você digitou um número inválido...\nDigite um número inteiro, positivo e maior que zero:`))
 }
-// verificar se o número é primo (pra ser primo precisará ser divisível por 1 e por ele mesmo)
-while (contador <= numero)
-resultado = numero % contador
-soma = soma + resultado
-contador++
-if (soma > 1){
-    console.log(`O número ${numero} é primo`)
+let resto, contador = 1, soma=0
+while(contador <= numero){
+    if (numero % contador === 0){
+        soma++
+    }
+    contador++
+}
+if (soma == 2){
+    console.log(`O NÚMERO ${numero} É PRIMO`)
 }
 else{
-    console.log(`O número não é primo`)
+    console.log(`O NÚMERO ${numero} NÃO É PRIMO`)
 }
