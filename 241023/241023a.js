7616518c2e15ac572e1fb2fdc2781ee47834a18b
jeandouglasjr <suporte@jeandouglas.com.br>
2024-10-23 21:01:23 -0300

    241023

--- 241023/241023a.js
+++ 241023/241023a.js
@@ -1,3 +1,19 @@
-function soma(a,b){
+function somar(a,b){
     return a+b
 }
+console.log(`Somar: ${somar(10,10)}`)
+
+function subtrair(a,b){
+    return a-b
+}
+console.log(`Subtrair: ${subtrair(10,10)}`)
+
+function multiplicar(a,b){
+    return a*b
+}
+console.log(`Multiplicar: ${multiplicar(10,10)}`)
+
+function dividir(a,b){
+    return a/b
+}
+console.log(`Dividir: ${dividir(10,10)}`)
