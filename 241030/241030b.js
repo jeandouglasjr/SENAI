ab50be3170cd430ae72d6dc190435d03c723b96d
jeandouglasjr <suporte@jeandouglas.com.br>
2024-10-30 22:07:59 -0300

    241030

---
+++ 241030/241030b.js
@@ -0,0 +1,11 @@
+// SENAI - Desenvolvimento de Sistemas - Lógica de Programação
+// Aluno: Jean Douglas Toledo Rodrigues Junior
+// Exercício 2
+
+// Utilize seus conhecimentos de Javascript para criar funções que convertam a temperatura:
+// Conversão de:
+// Celsius > Fahrenheit > °F = °C x 1,8 + 32
+// Fahrenheit > Celsius > °C = (°F - 32)/1,8
+// Celsius > Kelvin > K = °C + 273,15
+// Kelvin > Celsius > °C = K - 273,15
+
