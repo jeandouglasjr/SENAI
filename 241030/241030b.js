4d0870914c29972771542679db60eff33765af1e
jeandouglasjr <suporte@jeandouglas.com.br>
2024-10-31 21:33:44 -0300

    241030

--- 241030/241030b.js
+++ 241030/241030b.js
@@ -9,3 +9,30 @@
 // Celsius > Kelvin > K = °C + 273,15
 // Kelvin > Celsius > °C = K - 273,15
 
+function converter(){
+    let temperatura, indice, temperaturaInicial
+    indice = Number(prompt(`ESCOLHA A CONVERSÃO:\n[1] - Celsius (°C) > Fahrenheit (°F)\n[2] - Fahrenheit (°F) > Celsius (°C)\n[3] - Celsius (°C) > Kelvin (K)\n[4] - Kelvin (K) > Celsius (°C)`))
+    while(indice < 1 || indice > 4){
+        indice = prompt(Number(`Digite um número válido (entre 1 e 4)!\n[1] - Celsius (°C) > Fahrenheit (°F)\n[2] - Fahrenheit (°F) > Celsius (°C)\n[3] - Celsius (°C) > Kelvin (K)\n[4] - Kelvin (K) > Celsius (°C)`))
+    }
+    temperaturaInicial = Number(prompt(`Digite o valor da temperatura a converter:`))
+    switch(indice){
+        case 1:
+            temperatura = (temperaturaInicial * 1.8)+32
+            alert(`A temperatura ${temperaturaInicial} °C equivale a ${temperatura.toFixed(2)} °F.`)
+        break
+        case 2:
+            temperatura = (temperaturaInicial - 32)/1.8
+            alert(`A temperatura ${temperaturaInicial} °F equivale a ${temperatura.toFixed(2)} °C.`)
+        break
+        case 3:
+            temperatura = temperaturaInicial + 273.15
+            alert(`A temperatura ${temperaturaInicial} °C equivale a ${temperatura.toFixed(2)} K.`)
+        break
+        case 4:
+            temperatura = temperaturaInicial - 273.15
+            alert(`A temperatura ${temperaturaInicial} K equivale a ${temperatura.toFixed(2)} °C.`)
+        break
+    }
+}
+converter()
