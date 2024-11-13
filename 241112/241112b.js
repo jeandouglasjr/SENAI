215f9591656c8ae75632d45bc8123eea32be77fe
jeandouglasjr <suporte@jeandouglas.com.br>
2024-11-12 22:04:02 -0300

    241112

---
+++ 241112/241112b.js
@@ -0,0 +1,18 @@
+let listaNumero = [];
+let digitado =0
+let atual;
+let num = prompt("Digite quantos números queres digitar")
+for(let i=0;i<num;i++){
+    digitado = prompt("Digite o número")
+    listaNumero.push(digitado)
+}
+for(let i = 1; i < listaNumero.length; i++){
+    let j = i-1
+    atual = listaNumero[i];
+        while(j >= 0 && atual > listaNumero[j]){
+            listaNumero[j+1] = listaNumero[j];
+            j--
+        }
+    listaNumero[j+1] = atual
+}
+console.log(listaNumero)
