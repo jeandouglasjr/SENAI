74aed456a5fd15a61232dc60bfcbd2c925567c6a
jeandouglasjr <suporte@jeandouglas.oom.br>
2025-09-10 21:09:58 -0300

    250910

--- 2025/250909_mvc/src/index.js
+++ 2025/250909_mvc/src/index.js
@@ -1,46 +1,19 @@
-import express from "express"
-import ticketsRouters from "./routers/ticketsRouters"
+const express = require('express');
+// import ticketsRouters from "./routers/ticketsRouters"
 
-const app = express()
+const ticketsRouters = require('./routers/ticketsRouters');
+const app = express();
+app.use(express.json());
 
-const conexao = require('./connect')
+// rais
+app.get('/', (_, res) => res.send('API Tickets OK'));
 
-app.use(express.json())
-app.use(express.ticketsRouters)
+// rota de tickets
+app.use('/', ticketsRouters);
 
-/*
-Antes de refatorar (250909_mvcless)...
+// 404 de rota
+app.use((_, res) => res.status(404).json({ erro: 'Rota não encontrada' }));
 
-const express = require('express') - método require
-
-let tickets = []
-
-endpoints
-
-// get
-app.get('/', async (_, res) => {
-    // usar a variável conexão para rodar o comando SQL
-    const tickets = await conexao.query('select * from tickets')
-    res.status(200).send({ resposta: tickets.rows })
-})
-
-// post
-app.post('/', async (_, res) => {
-    const { nome, local, descricao, preco, foto } = req.body || {};
-    await conexao.query('insert into tickets(nome, local, descricao, preco, foto) values($1, $2, $3, $4, $5)', [nome, local, descricao, preco, foto])
-    res.status(201).send({ mensagem: 'Ticket cadastrado com sucesso' })
-})
-
-app.get('/tickets/:id', async (req, res) => {
-    const { id } = req.params || {};
-    const tickets = await conexao.query('select * from tickets where id = $1', [id])
-    if (!id) {
-        return res.status(404).json({
-            error: 'Ticket não encontrado'
-        });
-    }
-    return res.status(201).json(tickets.rows);
-});
-
-app.listen(3000, () => console.log('Servidor iniciado'))
-*/
+// inicia o servidor
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Servidor iniciado: http://localhost:${port}`));
