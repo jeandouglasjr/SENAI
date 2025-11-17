aeaa1732140294220c5c9b60f4d185670fc7d8a3
jeandouglasjr <suporte@jeandouglas.com.br>
2025-11-17 12:36:56 -0300

    251117

--- 2025/251111/back/app.js
+++ 2025/251111/back/app.js
@@ -1,36 +1,35 @@
-import express from 'express'
+import express from "express";
 
 // ativação do reconhecimento do dotenv (arquivo com variaveis de ambiente/credenciais)
-import 'dotenv/config'
+import "dotenv/config";
 // importar conexao do database
-import { conexao } from './database.js'
+import { conexao } from "./database.js";
 
-try{
-    await conexao.sync()
-}
-catch(error){
-    console.error(error)
+try {
+  await conexao.sync();
+} catch (error) {
+  console.error(error);
 }
 // importar arquivo de rotas
-import { routerUsuarios } from './routes/usuario.js'
+import { routerUsuarios } from "./routes/usuario.js";
 
-const app = express()
+const app = express();
 
 // Permitir o uso de JSON no body
-app.use(express.json())
+app.use(express.json());
 // Indicar o uso do arquivo de rotas
-app.use('/', routerUsuarios)
+app.use("/usuario", routerUsuarios);
 
 // Teste de conexão com o banco de dados
 try {
-    await conexao.authenticate()
-    // Comando que adiciona novas colunas nas tabelas
-    // await conexao.sync({ alter: true })
-    // Comando que exclui a tabela e seus dados e cria a tabela sem dados
-    // await conexao.sync({ force: true })
-    console.log('Conectado com sucesso')
-} catch(erro) {
-    console.log('Deu erro na conexao')
+  await conexao.authenticate();
+  // Comando que adiciona novas colunas nas tabelas
+  // await conexao.sync({ alter: true })
+  // Comando que exclui a tabela e seus dados e cria a tabela sem dados
+  // await conexao.sync({ force: true })
+  console.log("Conectado com sucesso");
+} catch (erro) {
+  console.log("Deu erro na conexão");
 }
 const port = process.env.PORT;
-app.listen(port, () => console.log(`http://localhost:${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`));
